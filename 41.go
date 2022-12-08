4b53d895b9b7101c16aa31976ae3842f80e1de34
lemonshwang <lemonshwang@tencent.com>
2022-12-08 13:24:32 +0800

    feat:1208

--- 41.go
+++ 41.go
@@ -1,5 +1,17 @@
 package main
 
 func firstMissingPositive(nums []int) int {
-
+	for index := 0; index < len(nums); {
+		if nums[index] <= 0 || nums[index] > len(nums) || nums[index] == index+1 || nums[index] == nums[nums[index]-1] {
+			index++
+		} else {
+			nums[index], nums[nums[index]-1] = nums[nums[index]-1], nums[index]
+		}
+	}
+	for i, v := range nums {
+		if v != i+1 {
+			return i + 1
+		}
+	}
+	return len(nums) + 1
 }
